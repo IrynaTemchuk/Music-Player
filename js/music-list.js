981a95baaeefe1c923dfc2fc8f0dacf328590a23
Iryna Temchuk <itemchuk98@gmail.com>
2023-03-27 18:04:10 +0300

    Add list of all songs

---
+++ js/music-list.js
@@ -0,0 +1,32 @@
+let allMusic = [
+    {
+        name: "Blinding Lights",
+        artist: "The Weekend",
+        img: "music-1",
+        src: "music-1",
+    },
+    {
+        name: "Modern World",
+        artist: "Max Jury",
+        img: "music-2",
+        src: "music-2",
+    },
+    {
+        name: "Sweater Weather",
+        artist: "The Neighbourhood",
+        img: "music-3",
+        src: "music-3",
+    },
+    {
+        name: "Empire State Of Mind",
+        artist: "Jay-Z feat. Alicia Keys",
+        img: "music-4",
+        src: "music-4",
+    },
+    {
+        name: "Save Me",
+        artist: "Kadebostany",
+        img: "music-5",
+        src: "music-5",
+    },
+]
