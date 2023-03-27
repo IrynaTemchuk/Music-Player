a6f6b689d4aa6f84331d3cc65acd26ad46eaa7ee
Iryna Temchuk <itemchuk98@gmail.com>
2023-03-27 18:31:15 +0300

    Add function play, paused, next

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 const wrapper = document.querySelector(".wrapper"),
 musicImg = wrapper.querySelector(".img-area img"),
 musicName = wrapper.querySelector(".song-details .name"),
-musicArtist = wrapper.querySelector(".song-details .artist");
+musicArtist = wrapper.querySelector(".song-details .artist"),
+mainAudio = wrapper.querySelector("#main-audio"),
+playPauseBtn = wrapper.querySelector(".play-pause"),
+prevBtn = wrapper.querySelector("#prev"),
+nextBtn = wrapper.querySelector("#next");
+
 
 let musicIndex = 2;
 
@@ -14,4 +19,49 @@ function loadMusic(indexNumb){
     musicName.innerText = allMusic[indexNumb - 1].name;
     musicArtist.innerText = allMusic[indexNumb - 1].artist;
     musicImg.src = `images/${allMusic[indexNumb - 1].img}.jpg`;
+    mainAudio.src = `songs/${allMusic[indexNumb - 1].src}.mp3`;
 }
+
+// play music function 
+function playMusic(){
+        wrapper.classList.add("paused");
+        playPauseBtn.querySelector("i").innerText = "pause";
+        mainAudio.play();
+}
+
+// pause music function 
+function pauseMusic(){
+    wrapper.classList.remove("paused");
+    playPauseBtn.querySelector("i").innerText = "play_arrow";
+    mainAudio.paused();
+}
+
+// next music function 
+function nextMusic(){
+    // here we'll just increment of index by 1
+    musicIndex++;
+    loadMusic(musicIndex);
+    playMusic();
+}
+
+// prev music function 
+function prevMusic(){
+
+}
+
+
+// play or music button event
+playPauseBtn.addEventListener('click', ()=>{
+    const isMusicPaused = wrapper.classList.contains("paused");
+    // if isMusicPaused is true then call pauseMusic else call playMusic
+    isMusicPaused ? pauseMusic() : playMusic();
+});
+
+// next music btn event
+nextBtn.addEventListener("click", ()=>{
+    nextMusic(); // calling next music function
+});
+
+prevBtn.addEventListener("click", ()=>{
+    prevMusic(); // calling prev music function
+});
